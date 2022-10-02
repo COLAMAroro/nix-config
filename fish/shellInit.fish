8505fcf7aa2db4071ec59652d9c84d9fb2c4daa0
Corentin Rondier <github@rondier.io>
2022-10-02 09:52:40 +0000

    Separated WSL2 with bare OS, added shellInits

--- fish/shellInit.fish
+++ fish/shellInit.fish
@@ -1,21 +1,32 @@
-status --is-interactive; and begin
+status --is-interactive; and begin # Make sure it's only run in interactive shell, it's mostly for nix-shells.
+# It could be ommited by using `programs.fish.interactiveShellInit` in home.nix
 
-if set -q PIPENV_ACTIVE
-    . $(pipenv --venv)/bin/activate.fish
+if ! functions -q envsource # Make sure you have a function source .env files
+    function envsource # https://gist.github.com/nikoheikkila/dd4357a178c8679411566ba2ca280fcc
+        for line in (cat $argv | grep -v '^#' | grep -v '^\s*$')
+            set item (string split -m 1 '=' $line)
+            set -gx $item[1] $item[2]
+            echo "Exported key $item[1]"
+        end
+    end
+    funcsave envsource
+end
+
+if set -q PIPENV_ACTIVE # If somehow you are in a pipenv env, but it's only activated for back (fuck `pipenv shell` in nix-shells)
+    . $(pipenv --venv)/bin/activate.fish # You want to activate if yourself for fish
     if test -e ".env"
         envsource ".env"
     end
-else if set -q VIRTUAL_ENV && test -e ./env/bin/activate.fish
+else if set -q VIRTUAL_ENV && test -e ./env/bin/activate.fish # We are still in a python environment, but not with pipenv, just vanilla venv
     . ./env/bin/activate.fish
     if test -e ".env"
         envsource ".env"
     end
 end
 
-if set -q IS_NODE_ENV
-    functions -c fish_prompt _old_fish_prompt
+if set -q IS_NODE_ENV # We are in a NodeJS environment (exported in a shellHook in the shell.nix)
+    functions -c fish_prompt _old_fish_prompt # Same old trick as venv, save the old prompt to do magic
     function fish_prompt
-        # Save the return status of the last command
         echo -n "(node) "
         _old_fish_prompt
     end
--- home.nix
+++ home.nix
@@ -1,6 +1,26 @@
 { config, pkgs, ... }:
 
-{
+let
+    plateformSpecificPackages = if (builtins.match ".*WSL2.*" (builtins.readFile /proc/version )) == null
+    then [# We are *not* in WSL
+      pkgs.teams
+      pkgs.discord
+      pkgs.vscode
+      pkgs.htop
+    ]
+    else [ # We *are* in WSL
+      pkgs.wslu
+    ];
+    
+    commonPackages = [
+      pkgs.fish
+      pkgs.openssh
+      pkgs.git
+      pkgs.file
+      pkgs.gh
+    ];
+
+in {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "cola";
@@ -21,20 +41,18 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.packages = [
-	pkgs.fish
-	pkgs.htop
-	pkgs.openssh
-	pkgs.git
-	pkgs.nodejs
-	pkgs.file
-#	pkgs.vscode
-	pkgs.gh
-  ];
+  home.packages = builtins.concatLists [ commonPackages plateformSpecificPackages ];
 
   programs.fish.enable = true;
   programs.fish.shellInit = builtins.readFile ./fish/shellInit.fish;
 
   programs.bash.enable = true;
   programs.bash.initExtra = ''source /home/cola/.config/nixpkgs/bash/bashrc'';
+
+  programs.git.enable = true;
+  programs.git.aliases = {
+  	tree = "log --all --decorate --oneline --graph";
+  };
+  programs.git.userEmail = "github@rondier.io";
+  programs.git.userName = "Corentin Rondier";
 }
