e4f0a5281d8785e10bad44872f52c812d34d95e1
COLAMAroro <github@rondier.io>
2022-12-26 16:06:09 +0100

    Add dconf for Gnome, some programs, reformat a bit
    Was added for dconf:
    Number of workspace
    All buttons on app menu
    Bind Alt+Tab for windows, not apps
    Add Alt+Shift+Espace for task manager
    
    Reformatting:
    SMTP Credentials are now an import (for rbw)
    Use ++ instead of builtins.concatLists
    "cls" and "nshell" alias for both fish and bash
    git config is now in a module
    WSL is now a variable; Firefox and dconf are now only -
    activated when is not in WSL
    
    XDG now create directories (in *english*)
    
    Programs:
    GitHub, Micro, htop and Bat are now managed
    Replace bitwarden-cli by rbw
    Adds navi, tealdeer and nnn

--- home.nix
+++ home.nix
@@ -9,14 +9,14 @@ let
   newDiscord = pkgs.discord.override {
     nss = newNss.nss_latest;
   };
+  isNotWSL = ((builtins.match ".*WSL2.*" (builtins.readFile /proc/version)) == null);
   plateformSpecificPackages =
-    if (builtins.match ".*WSL2.*" (builtins.readFile /proc/version)) == null
+    if (isNotWSL)
     then [
       # We are *not* in WSL
       pkgs.teams
       pkgs.discord
       pkgs.vscode
-      pkgs.htop
       pkgs.clip
       pkgs.remmina
       pkgs.libreoffice-fresh
@@ -38,23 +38,25 @@ let
     pkgs.fish
     pkgs.openssh
     pkgs.file
-    pkgs.gh
-    pkgs.micro
     pkgs.powershell
     pkgs.rnix-lsp
     pkgs.any-nix-shell
-    pkgs.bat
     pkgs.unzip
-    pkgs.bitwarden-cli
     pkgs.xclip
   ];
   bwSecrets = builtins.import ./bw.nix;
+  emailInfo = builtins.import ./smtpCredentials.nix;
 in
-{
+rec {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "cola";
   home.homeDirectory = "/home/cola";
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+  };
 
   nixpkgs.config.allowUnfree = true;
 
@@ -72,45 +74,90 @@ in
   programs.home-manager.enable = true;
 
   dconf = {
-    enable = true;
+    enable = isNotWSL;
     settings = {
       "org/gnome/shell".enabled-extensions = [
         "dash-to-panel@jderose9.github.com"
         "nightthemeswitcher-gnome-shell-extension@rmnvgr.gitlab.com"
       ];
+      "org/gnome/desktop/wm/preferences".num-workspaces = 1;
+      "org/gnome/desktop/wm/preferences".button-layout = "appmenu:minimize,maximize,close";
+      "org/gnome/desktop/wm/keybindings" = {
+        switch-application = "@as []";
+        switch-application-backward = "@as []";
+        switch-windows = "['<Alt>Tab']";
+        switch-windows-backward = "['<Shift><Alt>Tab']";
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        binding = "<Shift><Control>Escape";
+        command = "gnome-system-monitor";
+        name = "Task Manager";
+      };
     };
   };
 
-  home.packages = builtins.concatLists [ commonPackages plateformSpecificPackages ];
+  home.packages = commonPackages ++ plateformSpecificPackages;
 
   programs.fish.enable = true;
   programs.fish.shellInit = builtins.readFile ./fish/shellInit.fish;
-  programs.fish.shellAliases = { cat = "bat"; };
+  programs.fish.shellAliases = {
+    cls = "clear";
+    cat = "bat";
+    nshell = "nix-shell -p";
+  };
 
   programs.bash.enable = true;
   programs.bash.initExtra = ''source /home/cola/.config/nixpkgs/bash/bashrc'';
-  programs.bash.shellAliases = { cat = "bat"; };
+  programs.bash.shellAliases = programs.fish.shellAliases;
+
+  programs.bat.enable = true;
+  programs.bat.config = {
+    map-syntax = [
+      "flake.nix:json"
+    ];
+  };
+
+  programs.gh.enable = true;
+  programs.micro.enable = true;
+  programs.htop.enable = isNotWSL;
+  programs.navi.enable = true;
+  programs.tealdeer.enable = true;
+
+  programs.nnn.enable = true;
+  programs.nnn.package = pkgs.nnn.override ({ withNerdIcons = true; });
+  programs.nnn.extraPackages = with pkgs; [ ffmpegthumbnailer mediainfo sxiv ];
+
+  programs.rbw = {
+    enable = isNotWSL;
+    settings = {
+      email = emailInfo.email;
+      lock_timeout = 300;
+      pinentry = "gnome3";
+      base_url = "vault.bitwarden.com";
+    };
+  };
+
+  services.mpris-proxy.enable = isNotWSL;
 
   home.sessionVariables = {
     MOZ_WAYLAND =
-      if (builtins.getEnv "XDG_SESSION_TYPE" == "wayland")
+      if (builtins.getEnv "XDG_SESSION_TYPE" == "wayland" && isNotWSL)
       then 1 else 0;
     EDITOR = "micro";
-    BW_CLIENTID = bwSecrets.id;
-    BW_CLIENTSECRET = bwSecrets.secret;
-    BW_PASSWORD = bwSecrets.password;
   };
 
-  programs.git.enable = true;
-  programs.git.aliases = {
-    tree = "log --all --decorate --oneline --graph";
-    pall = "push --all";
+  programs.git = {
+    enable = true;
+    aliases = {
+      tree = "log --all --decorate --oneline --graph";
+      pall = "push --all";
+    };
+    userEmail = "github@rondier.io";
+    userName = "COLAMAroro";
+    extraConfig = {
+      sendemail = emailInfo;
+      init.defaultBranch = "main";
+    };
+    package = pkgs.gitFull;
   };
-  programs.git.userEmail = "github@rondier.io";
-  programs.git.userName = "COLAMAroro";
-  programs.git.extraConfig = {
-    sendemail = import ./smtpCredentials.nix;
-    init.defaultBranch = "main";
-  };
-  programs.git.package = pkgs.gitFull;
 }
